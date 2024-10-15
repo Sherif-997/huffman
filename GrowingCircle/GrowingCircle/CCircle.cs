48ed9135b0b491e8a4f0b24e369486c1fa171d81
Sherif-997 <sherifross997@gmail.com>
2024-10-16 00:12:51 +0300

    Add files via upload

---
+++ GrowingCircle/GrowingCircle/CCircle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowingCircle
+{
+    class CCircle
+    {
+        public float xc, yc;
+        public int radius;
+        public float stTheta;
+        public float endTheta;
+        public bool incir;
+
+        public CCircle(float xc, float yc, int radius, float stTheta, float endTheta)
+        {
+            this.xc = xc;
+            this.yc = yc;
+            this.radius = radius;
+            this.stTheta = stTheta * (float)Math.PI / 180;
+            this.endTheta = endTheta * (float)Math.PI / 180;
+        }
+        public void drawcircle(Graphics g)
+        {
+            float x, y;
+
+            for (float th = stTheta; th <= endTheta; th += 0.01f)
+            {
+                x = (float)(radius * Math.Cos(th) + xc);
+                y = (float)(radius * Math.Sin(th) + yc);
+                g.FillEllipse(Brushes.Red, x, y, 5, 5);
+                //g.DrawLine(Pens.Red, xc, yc, x, y);
+            }
+        }
+    }
+}
---
+++ GrowingCircle/GrowingCircle/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrowingCircle
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap off;
+        Timer timer = new Timer();
+        Random rr = new Random();
+
+        CCircle circle;
+        bool flag = false;
+        public Form1()
+        {
+            WindowState = FormWindowState.Maximized;
+            Load += Form1_Load;
+            Paint += Form1_Paint;
+            MouseDown += Form1_MouseDown;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(circle.radius < 150 && !flag)
+            {
+                circle.radius += 2;
+                
+                if(circle.radius >= 150)
+                {
+                    flag = true;
+                }
+            }
+            else if (circle.radius >= 40 && flag)
+            {
+                circle.radius -= 2;
+
+                if(circle.radius <= 40)
+                {
+                    flag = false;
+                }
+            }
+            DrawDubb(CreateGraphics());
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawDubb(CreateGraphics());
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            off = new Bitmap(ClientSize.Width, ClientSize.Height);
+
+            circle = new CCircle(ClientSize.Width/2, ClientSize.Height/2, 60, 0, 360);
+            
+            timer.Start();
+            DrawDubb(CreateGraphics());
+        }
+
+        void DrawDubb(Graphics g)
+        {
+            Graphics g2 = Graphics.FromImage(off);
+            DrawScene(g2);
+            g.DrawImage(off, 0, 0);
+        }
+
+        private void DrawScene(Graphics g)
+        {
+            g.Clear(Color.Black);
+
+            circle.drawcircle(g);
+        }
+    }
+}
